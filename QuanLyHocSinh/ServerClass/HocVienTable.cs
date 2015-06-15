0dd6f28cd9a478f2cc5d80f540b73124c4c59513
nthiep93 <nthiep93@gmail.com>
2015-06-15 11:37:00 +0700

    123
    123

---
+++ QuanLyHocSinh/ServerClass/HocVienTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerClass
+{
+    public class HocVienTable : Table
+    {
+        private HocVien hocvien;
+
+        public HocVienTable()
+        {
+            TableName = "HocVien";
+        }
+        public void Load(Control GridView)
+        {
+            ServerView.View.LoadTable(GridView, this.TableName);
+        }
+
+        //Làm tiếp đê
+    }
+}
