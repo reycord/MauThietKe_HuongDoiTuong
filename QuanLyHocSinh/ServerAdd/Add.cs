95cff30e614be312ca0254c1c5a79b84cfd85c9d
nthiep93 <nthiep93@gmail.com>
2015-06-15 11:42:54 +0700

    First Commit
    First Commit

---
+++ QuanLyHocSinh/ServerAdd/Add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ServerAdd
+{
+    public class Add
+    {
+        static public void AddTable(Control GridView,HocVien hv)// Add reference System.Window.Form
+        {
+            //Làm tiếp
+        }
+    }
+}
